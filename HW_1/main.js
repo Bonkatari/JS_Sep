8916b43f1f442d3d8c185d0c90a222bf27286575
Bonkatari <bonkatari@gmail.com>
2023-11-21 15:19:57 +0200

    hw 1 for js

---
+++ HW_1/main.js
@@ -0,0 +1,67 @@
+let s1 = 'hello';
+console.log(s1);
+// ----------------------------
+let s2 = 'owu';
+console.log(s2);
+// ----------------------------
+let s3= 'com';
+console.log(s3);
+// ----------------------------
+let s4= 'ua';
+console.log(s4);
+// ----------------------------
+let n1= 1;
+console.log(n1);
+// ----------------------------
+let n2= 10;
+console.log(n2);
+// ----------------------------
+let n3 = -999;
+console.log(n3);
+// ----------------------------
+let n4 = 123;
+console.log(n4);
+// ----------------------------
+let n5 = 3.14;
+console.log(n5);
+// ----------------------------
+let n6 = 2.7;
+console.log(n6);
+// ----------------------------
+let n7 = 16;
+console.log(n7);
+// ----------------------------
+let bool= 3 == '3';
+console.log(bool);
+// ----------------------------
+let bool2 = 6 < 3;
+console.log(bool2);
+// ----------------------------
+let firstName = 'Катерина';
+console.log(firstName);
+// ----------------------------
+let middleName = 'Олександрівна';
+console.log(middleName);
+// ----------------------------
+let lastName = 'Бондар';
+console.log(lastName);
+// ----------------------------
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+// ----------------------------
+let a= 100;
+console.log(typeof a);
+// ----------------------------
+let b = '100';
+console.log(typeof b);
+// ----------------------------
+let c = true;
+console.log(typeof c);
+// ----------------------------
+let name = prompt("Введіть ваше ім'я:", "Катерина");
+let nameMiddle = prompt("Введіть ваше По-Батькові:", "Олександрівна");
+let age = prompt("Введіть ваш вік:", "21");
+console.log("Ім'я: " + name);
+console.log("По-батькові: " + nameMiddle);
+console.log("Вік: " + age);
+// ----------------------------
